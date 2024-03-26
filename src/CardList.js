d4dc431813192d6736ece9bccc3ddcf82d5db0f6
Huong <huongnguyen.nth1709@gmail.com>
2024-03-26 22:56:25 +0200

    remove the parent node of the CardList component

--- src/CardList.js
+++ src/CardList.js
@@ -3,13 +3,13 @@ import Card from './Card';
 
 const CardList = ({ robots }) => {
   return (
-    <div>
+    <>
       {robots.map((robot, i) => {
         return (
           <Card key={i} id={robot.id} name={robot.name} email={robot.email} />
         );
       })}
-    </div>
+    </>
   );
 };
 
