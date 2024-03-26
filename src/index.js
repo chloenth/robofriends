19e1c217f2e43f39faf849bd3da377c59c9a71b9
Huong <huongnguyen.nth1709@gmail.com>
2024-03-26 16:59:18 +0200

    add CardList component

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Card from './Card';
+import 'tachyons';
+import CardList from './CardList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Card />
+    <CardList />
   </React.StrictMode>
 );
 
