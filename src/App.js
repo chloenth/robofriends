bdb73abe73cc690e5e09b9312162c0e8e9e1fca6
Huong <huongnguyen.nth1709@gmail.com>
2024-03-26 21:01:59 +0200

    add Search function and filter data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
+import SearchBox from './SearchBox';
 
 const App = () => {
+  const [data, setData] = useState({
+    robots,
+    searchField: '',
+  });
+
+  const onSearchChange = (e) => {
+    setData({ robots, searchField: e.target.value });
+  };
+
+  const filterRobots = data.robots.filter((robot) =>
+    robot.name.toLowerCase().includes(data.searchField.toLowerCase())
+  );
+
   return (
-    <>
-      <h1>RoboFriends</h1>
-      <CardList robots={robots} />
-    </>
+    <div className='tc'>
+      <h1 className='f1'>RoboFriends</h1>
+      <SearchBox searchChange={onSearchChange} />
+      <CardList robots={filterRobots} />
+    </div>
   );
 };
 
