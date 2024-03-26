abe0a5ea147560ae08aa62fdbdbf3a027feb461a
Huong <huongnguyen.nth1709@gmail.com>
2024-03-26 22:32:32 +0200

    add Effect hook to fetch data from external resource

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
 import SearchBox from './SearchBox';
 
 const App = () => {
   const [data, setData] = useState({
-    robots,
+    robots: [],
     searchField: '',
   });
 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.cypress.io/users')
+      .then((res) => res.json())
+      .then((users) => setData({ ...data, robots: users }));
+  }, []);
+
   const onSearchChange = (e) => {
-    setData({ robots, searchField: e.target.value });
+    setData({ ...data, searchField: e.target.value });
   };
 
   const filterRobots = data.robots.filter((robot) =>
     robot.name.toLowerCase().includes(data.searchField.toLowerCase())
   );
+
+  if (data.robots.length === 0) {
+    return <h1 className='tc'>Loading</h1>;
+  }
 
   return (
     <div className='tc'>
